3987a0c8fbf273dba9c3f32c7271f10f76369e23
Pramod Kashyap N R <Pramod.Ramanth@in.ey.com>
2018-10-25 12:21:20 +0530

    Second commit

---
+++ src/main/java/com/teamCity/teamCityJava/TeamCityJavaApplication.java
@@ -0,0 +1,12 @@
+package com.teamCity.teamCityJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCityJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCityJavaApplication.class, args);
+	}
+}
